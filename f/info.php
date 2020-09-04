d0c3ba6cdd30ea6eadc55ca16c6b3266fd286885
ChenMoncay <70624179+ChenMoncay@users.noreply.github.com>
2020-09-05 02:28:47 +0800

    Add files via upload

--- f/info.php
+++ f/info.php
@@ -1,27 +1,32 @@
 <html>
 <body>
 <?php
-		$con=mysql_connect("localhost","root","");
-		
-		if (!$con)
-		{
-			die('Could not connect: '.mysql_error());
-		}
-		
-		mysql_select_db("registrar_info", $con);
-		
-		$sql="INSERT INTO student_info (name, gender, yc, contact)
-		VALUES ('$_POST[name]','$_POST[gender]','$_POST[yc]','$_POST[contact]')";
-		
-		if (!mysql_query($sql,$con))
-		{
-			die('Error: '.mysql_error());
-		}
-		
-		echo "Record added.";
-		
-		mysql_close($con);
-		
+$database    = 'registrar_info';
+$username    = 'root';
+$host        = 'localhost';
+$password    = '';
+
+
+            $con = mysqli_connect($host,$username,$password,$database);
+
+        if (!$con)
+        {
+            die('Could not connect: '.mysql_error());
+        }
+
+        mysql_select_db("registrar_info", $con);
+
+        $sql="INSERT INTO student_info (name, gender, yc, contact)
+        VALUES ('$_POST[name]','$_POST[gender]','$_POST[yc]','$_POST[contact]')";
+
+        if (!mysql_query($sql,$con))
+        {
+            die('Error: '.mysql_error());
+        }
+
+        echo "Record added.";
+
+        mysql_close($con);
 ?>
 </body>
 </html>
