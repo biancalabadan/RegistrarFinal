ba59fc70ae45346fdaf445b4b90fadd0f33e9c8d
biancalabadan <70714674+biancalabadan@users.noreply.github.com>
2020-09-10 18:14:46 +0800

    Add files via upload

--- student_db.sql
+++ student_db.sql
@@ -1,41 +1,63 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.13-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 10, 2020 at 12:14 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `student_db`
+--
 
--- Dumping database structure for student_db
-CREATE DATABASE IF NOT EXISTS `student_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `student_db`;
+-- --------------------------------------------------------
 
--- Dumping structure for table student_db.students
-CREATE TABLE IF NOT EXISTS `students` (
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
   `student_id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `year` varchar(50) NOT NULL,
   `course` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `is_enrolled` varchar(50) NOT NULL,
-  `violation` varchar(50) NOT NULL,
-  PRIMARY KEY (`student_id`)
+  `violation` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table student_db.students: ~2 rows (approximately)
-DELETE FROM `students`;
-/*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` (`student_id`, `name`, `year`, `course`, `email`, `is_enrolled`, `violation`) VALUES
-	(1, 'Rey Christian Cute', '1st year', 'BS IT', 'sample@yahoo.com', 'Yes', 'Yes'),
-	(8, 'asdasd', 'sd', 'd', 'sdasd', 'Yes', 'Yes');
-/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+--
+-- Dumping data for table `students`
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+INSERT INTO `students` (`student_id`, `name`, `year`, `course`, `email`, `is_enrolled`, `violation`) VALUES
+(1, 'Rey Christian Cute', '1st year', 'BS IT', 'sample@yahoo.com', 'Yes', 'Yes'),
+(8, 'asdasd', 'sd', 'd', 'sdasd', 'Yes', 'Yes'),
+(234, 'Kathleen Pacanut', 'kath@yahoo.com', '3', 'BSIT', 'Yes', 'Yes'),
+(525, 'Hell Boy', 'hellish@yahoo.com', '4', 'BS BIOLOGY', 'Yes', 'None');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
